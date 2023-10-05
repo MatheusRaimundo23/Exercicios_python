97be58802784d0485ce37945cbef640b8e902330
MatheusRaimundo23 <mathfilho04@gmail.com>
2023-10-04 21:19:22 -0300

    Create exercicio_if_else.py

---
+++ exercicio_if_else.py
@@ -0,0 +1,7 @@
+primeiro_valor=input("digite um numero:")
+segundo_valor=input("digite outro numero:")
+
+if primeiro_valor > segundo_valor:
+     print(f"{primeiro_valor} é maior que {segundo_valor}")
+else:
+     print(f"{segundo_valor} é maior que {primeiro_valor}")
